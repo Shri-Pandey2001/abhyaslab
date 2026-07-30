8d5805f3b28a756bedb9a2915ba778647e359e4e
Shri-Pandey2001 <shridhar.edu@gmail.com>
2026-07-30 12:14:26 +0530

    Add Supabase frontend configuration

---
+++ js/supabase-config.js
@@ -0,0 +1,13 @@
+window.ABHYASLAB_CONFIG = Object.freeze({
+  supabaseUrl: "https://bchjqdcmnwsedczfpyuo.supabase.co",
+
+  publishableKey: "sb_publishable_jljfxn6cYKDfzhWd1qzcjQ_CvTg__hs",
+
+  functions: Object.freeze({
+    auth:
+      "https://bchjqdcmnwsedczfpyuo.supabase.co/functions/v1/abhyaslab-auth",
+
+    admin:
+      "https://bchjqdcmnwsedczfpyuo.supabase.co/functions/v1/abhyaslab-admin",
+  }),
+});
